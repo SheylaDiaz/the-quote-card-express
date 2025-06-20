c35481929644c41a71107ad854b937edc6f6116e
sheyla diaz <Dsheyla13@gmail.com>
2025-06-20 15:34:59 -0400

    Describe what changed

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ const elements = {
     author: document.getElementById("author"),
 };
 async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
+    const client_id = "bbohtb1sJylpkcqu-KsDatNqvhj0zkdjaPUOjs9sRxY";
     const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
     try {
         const response = await fetch(endpoint);
