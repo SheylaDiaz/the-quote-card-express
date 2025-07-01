21a2150905b469c2bc7e4cc905bbda01bdedf276
sheyla diaz <Dsheyla13@gmail.com>
2025-07-01 14:26:28 -0400

    update all

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 const express = require("express");
 const app = express();
-
+const PORT = process.env.PORT || 8080;
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
   console.log("Press Ctrl+C to end this process.");
