532103045644c4aa362bd0e02d6d00674dfae4a0
sheyla diaz <Dsheyla13@gmail.com>
2025-07-03 13:22:04 -0400

    update all

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 "use strict";
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 8080; 
 
 require("dotenv").config();
 const cors = require("cors");
+app.use(cors());
 const corsOptions = {
     origin: `http://localhost:${port}`
 }
-app.use(cors());
+
 app.use(cors(corsOptions));
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 async function getRandomImage() {
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    const endpoint = "https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}";
     try {
         const response = await fetch(endpoint);
         const returnedData = await response.json();
-        console.log("Returned data:", returnedData);
         const receivedPhotoUrl = returnedData.urls.regular;
 
         return receivedPhotoUrl;
@@ -28,7 +28,7 @@ async function getRandomImage() {
     }
 }
 
-app.use("/api/v1/getRandomImage", async (request, response) => {
+app.get("/api/v1/getRandomImage", async (request, response) => {
     response.status(200).json({
         status: 200,
         data: await getRandomImage(),
