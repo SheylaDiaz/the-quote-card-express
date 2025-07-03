86d7f1959ffcb2fcebc221f99995deac43f95f0d
sheyla diaz <Dsheyla13@gmail.com>
2025-07-03 00:35:02 -0400

    update all

--- server.js
+++ server.js
@@ -2,12 +2,38 @@
 
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const port = 8080;
+require("dotenv").config();
+const cors = require('cors');
+const corsOptions ={
+    origin: ['http//localhost:${port}'],
+
+};
+
+app.use (cors(corsOptions));
 app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+async function getRandomImage() {
+    const endpoint = http//api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID};
+    
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+ return receivedPhotoUrl;
+    } catch (error) {
+        console.error(error);
+    }
+}
+app.use("/api/v1/getRandomImage", async (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: await getRandomImage(),
+    });
+});
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
   console.log("Press Ctrl+C to end this process.");
 });
