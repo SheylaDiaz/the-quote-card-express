0b4484f596828a82147127a61a6063abeca9e6ea
sheyla diaz <Dsheyla13@gmail.com>
2025-07-03 05:32:13 -0400

    update all

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@
 
 async function getRandomImage() {
     const client_id = "bbohtb1sJylpkcqu-KsDatNqvhj0zkdjaPUOjs9sRxY0";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}&collections=317099`;;
 
     try {
         const response = await fetch(endpoint);
