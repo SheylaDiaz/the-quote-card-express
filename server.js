47ba7820cc4989bca6246bbe276174d4a1a28c75
sheyla diaz <Dsheyla13@gmail.com>
2026-02-26 13:13:27 -0500

    up

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+app.use("/api/v1/getRandomImage", (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: process.env.CLIENT_ID
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running http://localhost:${port}`);
     console.log("Press Ctrl+C to end this process.");
 });
+require("dotenv").config();
+const cors = require("cors");
+
+app.use(cors());
